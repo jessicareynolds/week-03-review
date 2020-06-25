daf294aa76477e3ceed1199e8fced9355e680436
jessicareynolds <reynolds.jessann@gmail.com>
2020-06-24 23:13:01 -0400

    Age calculator callback

---
+++ test/app.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+describe ('Age calculator', function() {
+    it ( 'Computes an age', function () {
+        const age = 29;
+        assert.equal(age, 30);
+
+    })
+    
+
+});
